b2047a4cf1d9d7ded413f671ec44f9456883f927
LubaRo <cnlmode@gmail.com>
2020-01-05 09:21:49 +0400

    add test for simple yaml

--- tests/GenDiffTest.php
+++ tests/GenDiffTest.php
@@ -13,14 +13,19 @@ class GenDiffTest extends TestCase
     protected function setUp(): void
     {
         $this->files_dir = __DIR__ . '/fixtures/';
-        $this->expectedResult = file_get_contents($this->files_dir . 'expected.json');
+        $this->expectedResult = file_get_contents($this->files_dir . 'expected.txt');
     }
 
     public function testGenDiff()
     {
-        $beforeFilePath = $this->files_dir . 'before.json';
-        $afterFilePath = $this->files_dir . 'after.json';
+        $beforeJson = $this->files_dir . 'before.json';
+        $afterJson = $this->files_dir . 'after.json';
 
-        $this->assertEquals($this->expectedResult, GenDiff\genDiff($beforeFilePath, $afterFilePath));
+        $this->assertEquals($this->expectedResult, GenDiff\genDiff($beforeJson, $afterJson));
+
+        $beforeYaml = $this->files_dir . 'before.yaml';
+        $afterYaml = $this->files_dir . 'after.yaml';
+
+        $this->assertEquals($this->expectedResult, GenDiff\genDiff($beforeYaml, $afterYaml));
     }
 }
