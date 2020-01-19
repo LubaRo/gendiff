75df27493b28cf7416801558b8b51fb3cffd8fd5
LubaRo <cnlmode@gmail.com>
2020-01-19 11:28:06 +0400

    format data to result string

--- src/GenDiff.php
+++ src/GenDiff.php
@@ -9,6 +9,7 @@ use function Differ\Parser\parseFile;
 
 define('VERSION', '1.0');
 define('DEFAULT_FORMAT', 'pretty');
+define('IDENTATION_SIZE', 4);
 
 function run()
 {
@@ -50,28 +51,29 @@ function genDiff($filePath1, $filePath2)
     return $formatResult;
 }
 
-function findDifference(array $parsedData1, array $parsedData2): array
+function findDifference(array $data1, array $data2): array
 {
-    $data1 = prepareData($parsedData1);
-    $data2 = prepareData($parsedData2);
-
     $diff = array_reduce(array_keys($data1), function ($acc, $key) use ($data1, $data2) {
         if (!isset($data2[$key])) {
             $acc[$key] = array(
-                'value' => $data1[$key],
+                'value' => is_string($data1[$key]) ? $data1[$key] : json_encode($data1[$key]),
                 'status' => 'removed'
             );
-        } elseif ($data1[$key] == $data2[$key]) {
+        } elseif (json_encode($data1[$key]) === json_encode($data2[$key])) {
             $acc[$key] = array(
-                'value' => $data1[$key],
+                'value' => is_string($data1[$key]) ? $data1[$key] : json_encode($data1[$key]),
                 'status' => 'notChanged'
             );
         } else {
-            $acc[$key] = array(
-                'valueBefore' => $data1[$key],
-                'valueAfter' => $data2[$key],
-                'status' => 'changed'
-            );
+            if (gettype($data1[$key]) == 'object' || gettype($data2[$key]) == 'object') {
+                $acc[$key] = findDifference((array) $data1[$key], (array) $data2[$key]);
+            } else {
+                $acc[$key] = array(
+                    'valueBefore' =>  is_string($data1[$key]) ? $data1[$key] : json_encode($data1[$key]),
+                    'valueAfter' =>  is_string($data2[$key]) ? $data2[$key] : json_encode($data2[$key]),
+                    'status' => 'changed'
+                );
+            }
         }
         
         return $acc;
@@ -80,7 +82,7 @@ function findDifference(array $parsedData1, array $parsedData2): array
     $newValues = array_diff_key($data2, $data1);
     foreach ($newValues as $key => $value) {
         $diff[$key] = array(
-            'value' => $value,
+            'value' => json_encode($value),
             'status' => 'new'
         );
     }
@@ -88,47 +90,68 @@ function findDifference(array $parsedData1, array $parsedData2): array
     return $diff;
 }
 
-function formatResult($diff)
+function formatResult($diff, $nestedLevel = 0)
 {
     if (empty($diff)) {
         return '{}';
     }
 
+    $identation = Strings\times(' ', IDENTATION_SIZE);
+    $leftIdentation = Strings\times($identation, $nestedLevel);
     $result = array();
-    $result[] = '{';
+
+    if (empty($nestedLevel)) {
+        $result[] = $leftIdentation . '{';
+    }
 
     foreach ($diff as $key => $data) {
-        switch ($data['status']) {
-            case 'notChanged':
-                $result[] = "   $key: {$data['value']}";
-                break;
-            case 'changed':
-                $result[] = " + $key: {$data['valueAfter']}";
-                $result[] = " - $key: {$data['valueBefore']}";
-                break;
-            case 'removed':
-                $result[] = " - $key: {$data['value']}";
-                break;
-            case 'new':
-                $result[] = " + $key: {$data['value']}";
-                break;
+        if (is_array($data) && !isset($data['status'])) {
+            $result[] = $leftIdentation . $identation . "$key: {";
+            $result[] = formatResult($data, $nestedLevel + 1);
+        } else {
+            switch ($data['status']) {
+                case 'notChanged':
+                    $result[] = formatResultRow($leftIdentation, ' ', $key, $data['value']);
+                    break;
+                case 'changed':
+                    $result[] = formatResultRow($leftIdentation, '+', $key, $data['valueAfter']);
+                    $result[] = formatResultRow($leftIdentation, '-', $key, $data['valueBefore']);
+                    break;
+                case 'removed':
+                    $result[] = formatResultRow($leftIdentation, '-', $key, $data['value']);
+                    break;
+                case 'new':
+                    $result[] = formatResultRow($leftIdentation, '+', $key, $data['value']);
+                    break;
+            }
         }
     }
 
-    $result[] = '}';
+    $result[] = $leftIdentation . '}';
 
     $str = Strings\toSentence($result, PHP_EOL, PHP_EOL);
     
     return $str;
 }
 
-function prepareData($data)
+function formatResultRow($leftIdentation, $operator, $key, $value)
 {
-    foreach ($data as &$value) {
-        if (is_bool($value) || is_null($value)) {
-            $value = var_export($value, true);
+    $decodedValue = json_decode($value, true);
+
+    if (!is_array($decodedValue)) {
+        $valueData = is_string($decodedValue) ? $decodedValue : $value;
+        $result = "$leftIdentation  $operator $key: $valueData";
+    } else {
+        $tmp[] = "$leftIdentation  $operator $key: {";
+
+        foreach ($decodedValue as $k => $v) {
+            $tmp[] = $leftIdentation . Strings\times(' ', IDENTATION_SIZE * 2) . $k . ': ' . $v;
         }
+
+        $tmp[] = $leftIdentation . Strings\times(' ', IDENTATION_SIZE) . '}';
+
+        $result = Strings\toSentence($tmp, PHP_EOL, PHP_EOL);
     }
 
-    return $data;
+    return $result;
 }
