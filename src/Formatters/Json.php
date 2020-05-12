d9b5034295838fc0fdd486194294484d41ad3aa2
LubaRo <cnlmode@gmail.com>
2020-05-12 10:24:36 +0400

    remove excess data

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -2,8 +2,6 @@
 
 namespace Differ\Formatters\Json;
 
-use const Differ\GenDiff\{STATUS_NEW, STATUS_REMOVED, STATUS_CHANGED, STATUS_UNCHANGED, STATUS_COMPLEX};
-
 function format($data)
 {
     $result = json_encode($data, JSON_PRETTY_PRINT);
